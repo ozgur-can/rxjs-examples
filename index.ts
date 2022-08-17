e42491820553355a8bab26796c0c9e7905b4b04d
ozgurdevo <ozgurcanaltinok@gmail.com>
2022-08-17 20:10:00 +0300

    add simple usage with fromEvent & timer

--- index.ts
+++ index.ts
@@ -1,9 +1,46 @@
 import './style.css';
 
-import { of, map, Observable } from 'rxjs';
+import { map, fromEvent, timer, Subscription } from 'rxjs';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
-  .subscribe(console.log);
+const randomRadius = () => Math.floor(Math.random() * 25);
+const randomAdd = () => Math.floor(Math.random() * add);
 
-// Open the console in the bottom right to see results.
+const timerObs = timer(0, 1);
+let timerSub: Subscription = null;
+const add = 5;
+
+// set initial border radius values to boxes
+const boxes = document.getElementsByClassName('box');
+[...boxes].forEach((box) => {
+  const radius = randomRadius();
+  box.style.borderRadius = `${radius}%`;
+  let radiusText = document.createTextNode(box.style.borderRadius);
+  box.appendChild(radiusText);
+});
+
+// mouse enter and leave events returns element itself
+const mouseEnter = fromEvent(boxes, 'mouseenter').pipe(
+  map((event) => event.target)
+);
+const mouseLeave = fromEvent(boxes, 'mouseleave').pipe(
+  map((event) => event.target)
+);
+
+// increase border radius when mouse entered box
+const mouseEnterObs = mouseEnter.subscribe((element: HTMLDivElement) => {
+  timerSub = timerObs.subscribe((timer) => {
+    let radius = parseInt(element.style.borderRadius);
+    // update radius or stop timer
+    if (radius >= 50) {
+      timerSub.unsubscribe();
+    } else {
+      element.style.borderRadius = `${radius + randomAdd()}%`;
+      element.textContent = element.style.borderRadius;
+    }
+  });
+});
+
+// stop timer when mouse is leaves box
+const mouseLeaveObs = mouseLeave.subscribe((element) => {
+  timerSub.unsubscribe();
+});
